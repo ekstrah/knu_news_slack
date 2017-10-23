91c98aea0406fccaf0ba09be8489547b199495ab
Unknown <ekstrah2013@knu.ac.kr>
2017-10-24 01:31:47 +0900

    Error fix
    TempLink Out of range fix

--- knuWeb.py
+++ knuWeb.py
@@ -23,61 +23,53 @@ class knuNews_Slack_Bridge:
 		targetPage_html = targetClient.read()
 		targetClient.close()
 		targetPage_soup = bsoup(targetPage_html, "html.parser")
-		comp_news = targetPage_soup.findAll("div", {'id':'myContainer'});
+		#comp_news = targetPage_soup.findAll("div", {'id':'myContainer'});
+		comp_news = targetPage_soup.findAll("tbody")
 
-		for list in comp_news:
-			for news in list.findAll("a", title=True):
-				self.tempLink.append(str(news))
-				tempTitle = str(news.get('title'))
-				tempHref = str(news.get('href'))
-				self.tempLink_title.append(tempTitle)
-				self.tempLink_href.append(tempHref)
-				print(tempHref)
-				print(tempTitle)
-				
+		for listLinks in comp_news:
+			for news in listLinks.findAll("a"):
+				print(news.text)
+				self.tempLink_title.append(news.text)		
 
-	def initialJsonUpdate(self):
+	# def initialJsonUpdate(self):
 		
-		for numLink in range(0, len(self.tempLink)):
-			time.sleep(5)
-			f = open("temp.json", "w")
-			f.write('''{
-					"username" : "KNU_NEWS_FEED",
-					"icon_emoji" : ":ghost:",
-					"attachments" : [
-					{
-						"color":"danger",
-						"fields" : [
-						{
-							"title":"''' + self.tempLink_title[numLink] + '''",
-							"value": "'''+ target_url + self.tempLink_href[numLink] +'''",
-							"short":false
-						}
-						]
-					}
-					]
-				}''')
+	# 	for numLink in range(0, len(self.tempLink)):
+	# 		time.sleep(5)
+	# 		f = open("temp.json", "w")
+	# 		f.write('''{
+	# 				"username" : "KNU_NEWS_FEED",
+	# 				"icon_emoji" : ":ghost:",
+	# 				"attachments" : [
+	# 				{
+	# 					"color":"danger",
+	# 					"fields" : [
+	# 					{
+	# 						"title":"''' + self.tempLink_title[numLink] + '''",
+	# 						"value": "'''+ target_url + self.tempLink_href[numLink] +'''",
+	# 						"short":false
+	# 					}
+	# 					]
+	# 				}
+	# 				]
+	# 			}''')
 
-			command = """curl -X POST -H 'Content-type: application/json' --data @temp.json https://hooks.slack.com/services/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"""
-			p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
-			out, err = p.communicate()	
+	# 		command = """curl -X POST -H 'Content-type: application/json' --data @temp.json https://hooks.slack.com/services/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"""
+	# 		p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
+	# 		out, err = p.communicate()	
 
 
 
 	def compareNewFeeds(self):
-		for i in range(0, len(self.newLink)):
+		k = 1
+		for i in range(0, len(self.newLink_title)):
 			#time.sleep(1)
-			print(self.newLink[i])
-			print(self.tempLink[i])
 
-			if self.newLink[i] == self.tempLink[i]:
-				print("they are same")
+			if self.newLink_title[i] == self.tempLink_title[i]:
+				k += 1
 
 			else:
 				print("new Update")
-				print(self.newLink[i])
-				self.updatedNews = self.newLink_title[i]
-				self.updatedHref = self.target_url + self.newLink_href[i]
+				print(self.newLink_title[i])
 				return 1
 		return 2
 
@@ -94,62 +86,59 @@ class knuNews_Slack_Bridge:
 		newPage_html = newClient.read()
 		newClient.close()
 		newPage_soup = bsoup(newPage_html, "html.parser")
-		new_comp_news = newPage_soup.findAll("div", {"id":'myContainer'});
+		new_comp_news = newPage_soup.findAll('tbody');
 
 		for new_list in new_comp_news:
-			for new_news in new_list.findAll("a", title=True):
-				self.newLink.append(str(new_news))
-				newTitle = str(new_news.get('title'))
-				newHref = str(new_news.get('href'))
-				self.newLink_title.append(newTitle)
-				self.newLink_href.append(newHref)
-				print(new_news)
+			for new_news in new_list.findAll("a"):
+				self.newLink_title.append(new_news.text)
 
-	def export_json_updated(self):
-		for numLink in range(0, len(self.tempLink)):
-			time.sleep(5)
-			f = open("temp.json", "w")
-			f.write('''{
-					"username" : "KNU_NEWS_FEED",
-					"icon_emoji" : ":ghost:",
-					"attachments" : [
-					{
-						"color":"danger",
-						"fields" : [
-						{
-							"title":"''' + self.updatedNews + '''",
-							"value": "'''+ self.updateHref +'''",
-							"short":false
-						}
-						]
-					}
-					]
-				}''')
 
-			command = """curl -X POST -H 'Content-type: application/json' --data @temp.json https://hooks.slack.com/services/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"""
-			p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
-			out, err = p.communicate()	
+	# def export_json_updated(self):
+	# 	for numLink in range(0, len(self.tempLink)):
+	# 		time.sleep(5)
+	# 		f = open("temp.json", "w")
+	# 		f.write('''{
+	# 				"username" : "KNU_NEWS_FEED",
+	# 				"icon_emoji" : ":ghost:",
+	# 				"attachments" : [
+	# 				{
+	# 					"color":"danger",
+	# 					"fields" : [
+	# 					{
+	# 						"title":"''' + self.updatedNews + '''",
+	# 						"value": "'''+ self.updateHref +'''",
+	# 						"short":false
+	# 					}
+	# 					]
+	# 				}
+	# 				]
+	# 			}''')
+
+	# 		command = """curl -X POST -H 'Content-type: application/json' --data @temp.json https://hooks.slack.com/services/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"""
+	# 		p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
+	# 		out, err = p.communicate()	
 
 
 	def testify(self):
-		print(self.newLink)
-		print(self.tempLink)
+		print(self.newLink_title)
+		print(self.tempLink_title)
 		print("test Finished")
 			
 	
 if __name__ == "__main__":
 
-	target_url = 'http://computer.knu.ac.kr/07_sub/01_sub.html'
+	target_url = 'http://www.ilbe.com/ilbe'
 	a = 2
 
 	knuParser = knuNews_Slack_Bridge(target_url)
 	knuParser.testify()	
-	knuParser.initialJsonUpdate()
+	# knuParser.initialJsonUpdate()
 
 	while(1):
 		knuParser.checkNewFeed()
 		a = knuParser.compareNewFeeds()
-		if a is 1:
-			knuParser.updateLinks()
-		time.sleep(1800)
-
+		print("sleeping for 10 seconds")
+		time.sleep(10)
+		knuParser.tempLink_title = knuParser.newLink_title
+		knuParser.newLink_title = []
+		print("begin waky waky")
